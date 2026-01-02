04292f2ad3a54ab833c41bd8e34ec276e2ed7d33
Kube-ZEN: Zero-Effort KuberNetes <zen@kube-zen.io>
2026-01-02 17:45:42 -0500

    fix: Remove last env.client reference

--- test/integration/experimental_features_test.go
+++ test/integration/experimental_features_test.go
@@ -73,10 +73,6 @@ func TestExperimentalFeatures_JSONv2(t *testing.T) {
 		},
 	}
 
-	if err := env.client.Create(ctx, jobFlow); err != nil {
-		t.Fatalf("Failed to create JobFlow: %v", err)
-	}
-
 	// Note: In real integration tests, we'd create the JobFlow in a cluster
 	// For experimental feature tests, we focus on JSON/GC performance
 	// Test JSON marshaling performance (simulating status updates)
